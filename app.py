ce565741e8b4104eed9bba174970a581b87391bf
talentrics <daniel@talentrics.com>
2020-10-27 12:31:19 -0400

    chng

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import dash_html_components as html
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+df = pd.read_excel(
+    "https://github.com/chris1610/pbpython/blob/master/data/salesfunnel.xlsx?raw=True"
+)
+mgr_options = df["Manager"].unique()
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
@@ -13,10 +18,53 @@ This is my first deployed app
 '''
 
 app.layout = html.Div([
-
-    dcc.Markdown(children=top_markdown_text),
-
+    html.H2("Sales Funnel Report"),
+    html.Div(
+        [
+            dcc.Dropdown(
+                id="Manager",
+                options=[{
+                    'label': i,
+                    'value': i
+                } for i in mgr_options],
+                value='All Managers'),
+        ],
+        style={'width': '25%',
+               'display': 'inline-block'}),
+    dcc.Graph(id='funnel-graph'),
 ])
+
+
+@app.callback(
+    dash.dependencies.Output('funnel-graph', 'figure'),
+    [dash.dependencies.Input('Manager', 'value')])
+def update_graph(Manager):
+    if Manager == "All Managers":
+        df_plot = df.copy()
+    else:
+        df_plot = df[df['Manager'] == Manager]
+
+    pv = pd.pivot_table(
+        df_plot,
+        index=['Name'],
+        columns=["Status"],
+        values=['Quantity'],
+        aggfunc=sum,
+        fill_value=0)
+
+    trace1 = go.Bar(x=pv.index, y=pv[('Quantity', 'declined')], name='Declined')
+    trace2 = go.Bar(x=pv.index, y=pv[('Quantity', 'pending')], name='Pending')
+    trace3 = go.Bar(x=pv.index, y=pv[('Quantity', 'presented')], name='Presented')
+    trace4 = go.Bar(x=pv.index, y=pv[('Quantity', 'won')], name='Won')
+
+    return {
+        'data': [trace1, trace2, trace3, trace4],
+        'layout':
+        go.Layout(
+            title='Customer Order Status for {}'.format(Manager),
+            barmode='stack')
+    }
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
