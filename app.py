49c88cb8678841cdefb36dea8f80e4bd5ea0a013
talentrics <daniel@talentrics.com>
2020-10-27 12:37:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+import pandas as pd
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 df = pd.read_excel(
