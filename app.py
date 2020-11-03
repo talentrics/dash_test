5d8e945610506e70eed7ba4d1e4d9128a7972985
talentrics <daniel@talentrics.com>
2020-11-03 15:59:38 -0500

    changes

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
+tsne_2d = pd.read_csv('static/onet_tsne.csv')
+
 app.layout = html.Div([
     html.H2('Hello World'),
     dcc.Dropdown(
@@ -22,8 +24,15 @@ app.layout = html.Div([
 
 @app.callback(dash.dependencies.Output('display-value', 'children'),
               [dash.dependencies.Input('dropdown', 'value')])
+
 def display_value(value):
-    return 'You have selected "{}"'.format(value)
+    fig = px.scatter(tsne_2d.dropna().rename(columns={0:'dim_0',1:'dim_1'}),
+                 x='dim_0',y='dim_1',hover_data={'sqrt_emp':False,'dim_0':False,
+                                                          'dim_1':False,'emp_2018':False},
+                 hover_name='occ',size='sqrt_emp',
+                 title='Occupational Landscape',width=1000, height=700)
+    fig.update_layout(hoverlabel={'bgcolor':'white','font_size':16,'font_family':'Rockwell'})
+    return fig.show()
 
 if __name__ == '__main__':
     app.run_server(debug=True)
