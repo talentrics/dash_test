64d57f01f6dd9558e8ea6a90e4d389fa35e8946d
talentrics <daniel@talentrics.com>
2020-11-04 13:49:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,21 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+import plotly.express as px
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-df = pd.read_csv('static/salesfunnel.csv')
+tsne_2d = pd.read_csv('static/onet_tsne.csv')
+
+fig = px.scatter(tsne_2d.dropna().rename(columns={0:'dim_0',1:'dim_1'}),
+                 x='dim_0',y='dim_1',hover_data={'sqrt_emp':False,'dim_0':False,
+                                                          'dim_1':False,'emp_2018':False},
+                 hover_name='occ',size='sqrt_emp',
+                 title='Occupational Landscape',width=1000, height=700)
 
 app.layout = html.Div([
     html.H2('Hello World'),
@@ -21,6 +29,7 @@ app.layout = html.Div([
         value='LA'
     ),
     html.Div(id='display-value')
+    dcc.Graph(figure=fig)
 ])
 
 @app.callback(dash.dependencies.Output('display-value', 'children'),
