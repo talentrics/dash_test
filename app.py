162d5ebd2897a9e1d5b541094bf9e8a1d07e35fc
talentrics <daniel@talentrics.com>
2020-11-11 11:33:09 -0500

    changes

--- app.py
+++ app.py
@@ -13,23 +13,22 @@ server = app.server
 
 tsne_2d = pd.read_csv('static/onet_tsne.csv')
 dis_activities = pd.read_csv('static/dis_activities.csv')
+test_case = pd.read_csv('static/test_case.csv')
 
 pick_list = list(tsne_2d.occ)
 
-top5 = {}
-
-for occ in dis_activities:
-    
-    temp = dis_activities['occ'].sort_values().reset_index().merge(
-        tsne_2d.groupby('occ')[['dim_0','dim_1','emp_2018','wage','sqrt_emp'
-                           ]].mean(),on='occ',how='inner').head(11)
-    top5[occ] = temp
-
-print(top5)
+fig = px.scatter(tsne_2d.dropna().rename(columns={0:'dim_0',1:'dim_1'}),
+                 x='dim_0',y='dim_1',hover_data={'sqrt_emp':False,'dim_0':False,
+                                                          'dim_1':False,'emp_2018':False},
+                 hover_name='occ',size='sqrt_emp',
+                 title='Occupational Landscape',width=1000, height=700)
+    fig.update_layout(hoverlabel={'bgcolor':'white','font_size':16,'font_family':'Rockwell'})
 
 app.layout = html.Div([
     
     html.H2('Testing my app - choose an occupation to get nearest neighbors'),
+
+    dcc.Graph(fig)
 
     dcc.Dropdown(
         id='dropdown',
@@ -56,20 +55,15 @@ def update_graph(option_slctd):
     print(type(option_slctd))
 
     container = "The value chosen by user was: {}".format(option_slctd)
-    
-    
 
-    new_neighbors = top5[option_slctd]
-
-    fig = px.scatter(new_neighbors,x='dim_0',y='dim_1',
+    fig2 = px.scatter(test_case,x='dim_0',y='dim_1',
                  hover_data={'sqrt_emp':False,'dim_0':False,
                             'dim_1':False,'emp_2018':False},
                  hover_name='occ',size='wage',
                  title='Occupational Landscape',width=500, height=350)
+    fig2.update_layout(hoverlabel={'bgcolor':'white','font_size':12,'font_family':'Rockwell'})
 
-    fig.update_layout(hoverlabel={'bgcolor':'white','font_size':12,'font_family':'Rockwell'})
-
-    return container, fig
+    return container, fig2
 
 if __name__ == '__main__':
     app.run_server(debug=False)
